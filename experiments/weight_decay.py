7e18a1e25587fee15241b9cb889e87be65a3a88a
wangyashuu <wangyashuu@gmail.com>
2022-02-04 21:41:25 +0000

    fix: add inception weight decay

--- experiments/weight_decay.py
+++ experiments/weight_decay.py
@@ -2,9 +2,13 @@ import sys
 
 import pandas as pd
 import wandb
+import torch
 
 from petfinder_pawpularity.config import create_config, to_flat_dict
-from petfinder_pawpularity.run import train_folds
+from petfinder_pawpularity.run import (
+    train_folds,
+    init_default_model,
+)
 from petfinder_pawpularity.util import add_path_to_data
 
 
@@ -20,6 +24,51 @@ def run_weight_decay(wandb_params, data, weight_decay=1e-1):
     run.finish()
 
 
+def run_inception_v3_weight_decay(
+    wandb_params, data, weight_decay=1e-1, beta=1
+):
+    def init_model(config):
+        def modify_model_with_aux(model):
+            num_aux_input_features = model.AuxLogits.fc.in_features
+            model.AuxLogits.fc = torch.nn.Linear(num_aux_input_features, 1)
+            return model
+
+        def modify_criterion_with_aux(criterion):
+            def loss_function(outs, y):
+                outputs, aux_outputs = outs
+                loss = criterion(outputs, y)
+                if aux_outputs is None:
+                    return loss
+                return loss + beta * criterion(aux_outputs, y)
+
+            return loss_function
+
+        return init_default_model(
+            config,
+            modify_model=modify_model_with_aux,
+            modify_loss_function=modify_criterion_with_aux,
+            aux_logits=True,
+        )
+
+    config = create_config(
+        dict(
+            n_folds=5,
+            model_name="inception_v3",
+            transform=dict(image_size=299),
+            optimizer=dict(params=dict(weight_decay=weight_decay)),
+            aux_beta=beta,
+        )
+    )
+    run = wandb.init(
+        group=f"experiment-inception-weight-decay-{weight_decay}",
+        reinit=True,
+        config=to_flat_dict(config),
+        **wandb_params,
+    )
+    train_folds(config, data, init_model=init_model)
+    run.finish()
+
+
 def run(wandb_entity, wandb_project, wandb_mode=None):
     data_path = "../input/petfinder-pawpularity-score/train"
     data = add_path_to_data(pd.read_csv(f"{data_path}.csv"), data_path)
@@ -29,8 +78,8 @@ def run(wandb_entity, wandb_project, wandb_mode=None):
         project=wandb_project,
         mode=wandb_mode,
     )
-    for weight_decay in [0, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]:
-        run_weight_decay(wandb_params, data, weight_decay)
+    for weight_decay in [0, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 3e-1, 1, 3]:
+        run_inception_v3_weight_decay(wandb_params, data, weight_decay)
 
 
 if __name__ == "__main__":
--- notes/experiment_weight_decay.md
+++ notes/experiment_weight_decay.md
@@ -14,6 +14,8 @@ experiments on with weight decay
 
 ## Result
 
+### Swin
+
 - [no weight decay, 18.021](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/36t2rnlj)
 - [weight decay=1e-5, 18.019](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/22ctqjvk)
 - [weight decay=1e-4, 18.019](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/18d0zt53)
@@ -21,6 +23,31 @@ experiments on with weight decay
 - [weight decay=1e-2, 18.021](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2e8hvmcb)
 - [weight decay=1e-1, 18.025](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/ioj70h26)
 
+### Inception
+
+- [no weight decay, 18.455](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3f9uu1e7)
+- [weight decay=1e-5, 18.481](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/16env8g5)
+- [weight decay=1e-4, 18.473](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3txfrevo)
+- [weight decay=1e-3, 18.493](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/9kjxynya)
+- [weight decay=1e-2, 18.508](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/1e70zft2)
+- [weight decay=1e-1, 18.457](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/34959mjx)
+- [weight decay=1, 18.487](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2aj0ssi0)
+- [weight decay=10, 18.369](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2ozu3z7h)
+- [weight decay=100, 19.244](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2cz1xtx8)
+
+### Inception with batch
+
+- [0, 18.5548 = (18.696+18.695+18.529+18.331+18.523)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/16evehsu)
+- [0.1, 18.565 = (18.716+18.708+18.535+18.33+18.536)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/1l3zz89i)
+- [0.3, 18.6674 = (18.898+18.909+18.629+18.321+18.58)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3a4y8pf0)
+- [1, 18.679 = (18.923+18.913+18.639+18.328+18.592)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2erx6bln)
+- [3, 18.6686 = (18.875+18.913+18.672+18.294+18.589)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/ieq8gvsf)
+- [10, 18.5434 = (18.665+18.685+18.515+18.366+18.486)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/jucr76hx)
+- [30, 18.6762 = (18.782+18.912+18.686+18.362+18.639)/5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/t0tvwazz)
+
 ## Conclusion
 
-It does not seem to help
+It does not seem to help. weight decay can be
+
+- swin 1e-4
+- inception 10
