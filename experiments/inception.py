05cc4e1f6f4dfd614e950581ae746322fa6d6d36
wangyashuu <wangyashuu@gmail.com>
2022-02-04 21:49:51 +0000

    add: inception exp

---
+++ experiments/inception.py
@@ -0,0 +1,64 @@
+import sys
+
+import pandas as pd
+import wandb
+import torch
+
+from petfinder_pawpularity.config import create_config, to_flat_dict
+from petfinder_pawpularity.run import train_folds, init_default_model
+from petfinder_pawpularity.util import add_path_to_data
+
+
+def run_inception_v3(wandb_params, data, beta):
+    def init_model(config):
+        def modify_model_with_aux(model):
+            num_aux_input_features = model.AuxLogits.fc.in_features
+            model.AuxLogits.fc = torch.nn.Linear(num_aux_input_features, 1)
+            return model
+
+        def modify_criterion_with_aux(criterion):
+            def loss_function(outs, y):
+                outputs, aux_outputs = outs
+                loss = criterion(outputs, y)
+                if aux_outputs is None:
+                    return loss
+                return loss + beta * criterion(aux_outputs, y)
+
+            return loss_function
+
+        return init_default_model(
+            config,
+            modify_model=modify_model_with_aux,
+            modify_loss_function=modify_criterion_with_aux,
+            aux_logits=True,
+        )
+
+    config = create_config(
+        dict(
+            model_name="inception_v3",
+            transform=dict(image_size=299),
+            aux_beta=beta,
+        )
+    )
+    run = wandb.init(config=to_flat_dict(config), reinit=True, **wandb_params)
+    train_folds(config, data, init_model=init_model)
+    run.finish()
+
+
+def run(wandb_entity=None, wandb_project=None, wandb_mode=None):
+    data_path = "../input/petfinder-pawpularity-score/train"
+    data = add_path_to_data(pd.read_csv(f"{data_path}.csv"), data_path)
+
+    wandb_params = dict(
+        entity=wandb_entity,
+        project=wandb_project,
+        mode=wandb_mode,
+    )
+    for beta in [3, 5, 10]:
+        run_inception_v3(wandb_params, data, beta=beta)
+
+
+if __name__ == "__main__":
+    wandb_entity = sys.argv[1] if len(sys.argv) > 1 else None
+    wandb_project = sys.argv[2] if len(sys.argv) > 2 else None
+    run(wandb_entity, wandb_project)
---
+++ notes/experiment_inception.md
@@ -0,0 +1,28 @@
+# Inception
+
+## Status
+
+DONE
+
+## Motivation
+
+If inception can perform better
+
+## Desciption
+
+experiments on different beta of auxiliary network
+
+## Result
+
+- [beta=10, 18.471](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2p4gcswf)
+- [beta=3, 18.513](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/ajoat1r6)
+- [beta=1, 18.489](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3l225rw8)
+- [beta=0.8, 18.514](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/1f6v28qx)
+- [beta=0.6, 18.5](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/2k7et7sm)
+- [beta=0.4, 18.515](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/82vxtat6)
+- [beta=0.2, 18.54](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3t20mnxr)
+- [beta=0, 18.569](https://wandb.ai/wangyashuu/PetfinderPawpularity/runs/3cy49m6q)
+
+## Conclusion:
+
+- no much different
